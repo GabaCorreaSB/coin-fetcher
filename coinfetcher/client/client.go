f0faa4bd942f95b49163b17d5770612928ab55d7
Gabriel Correa <gabacorrea@protonmail.com>
2023-09-26 03:07:24 -0300

    Improve the client and add some comments

--- coinfetcher/client/client.go
+++ coinfetcher/client/client.go
@@ -1,7 +1,4 @@
-//
-// Copyright (c) 2023 Gabriel Correa <gabriel.correasb@protonmail.com>
-//
-
+// This section specifies the package name and imports required for the code.
 package client
 
 import (
@@ -13,41 +10,52 @@ import (
 	"coinfetcher/types"
 )
 
+// Client represents a client for fetching cryptocurrency prices.
 type Client struct {
-	endpoint string
+	endpoint string // The endpoint URL for the price service.
 }
 
+// New creates a new instance of the Client with the specified endpoint.
 func New(endpoint string) *Client {
 	return &Client{
 		endpoint: endpoint,
 	}
 }
 
+// FetchPrice fetches cryptocurrency price information for the given ticker.
 func (c *Client) FetchPrice(ctx context.Context, ticker string) (*types.PriceResponse, error) {
+	// Create the full endpoint URL by combining the base endpoint and ticker parameter.
 	endpoint := fmt.Sprintf("%s?ticker=%s", c.endpoint, ticker)
 
-	req, err := http.NewRequest("get", endpoint, nil)
+	// Create an HTTP GET request to the endpoint.
+	req, err := http.NewRequest("GET", endpoint, nil)
 	if err != nil {
 		return nil, err
 	}
 
+	// Send the HTTP request using the default HTTP client.
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
 	}
 
+	// Check if the response status code is not OK (200).
 	if resp.StatusCode != http.StatusOK {
-		httpErr := map[string]any{}
+		// Decode the response body into a map for error information.
+		httpErr := map[string]interface{}{}
 		if err := json.NewDecoder(resp.Body).Decode(&httpErr); err != nil {
 			return nil, err
 		}
-		return nil, fmt.Errorf("service responsed with non OK status code: %s", httpErr["error"])
+		// Return an error with the service response error message.
+		return nil, fmt.Errorf("service responded with non-OK status code: %s", httpErr["error"])
 	}
 
+	// Create a new instance of PriceResponse to hold the decoded JSON response.
 	priceResp := new(types.PriceResponse)
 	if err := json.NewDecoder(resp.Body).Decode(priceResp); err != nil {
 		return nil, err
 	}
 
+	// Return the successfully fetched PriceResponse.
 	return priceResp, nil
 }
